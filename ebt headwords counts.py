9feb284371086c2979cac197fdbc4f3325561027
bdhrs <bodhirasa@gmail.com>
2022-07-29 08:17:50 +0530

    tweaks

--- ebt headwords counts.py
+++ ebt headwords counts.py
@@ -48,10 +48,13 @@ def setup():
 	sandhi_matches_dict_df = pd.DataFrame.from_dict(sandhi_matches_dict, orient="index")
 	sandhi_matches_dict_df.to_csv("output/sandhi_matches.csv", sep="\t", header =None)
 
-	# make headwords pos count dict
+	# make headwords pos count dict\
+	# and roots count dict
 
 	print(f"{timeis()} {white}5\theadwords pos count dict")
+	print(f"{timeis()} {white}6\troots count dict")
 	headwords_pos_count_dict = {}
+	roots_count_dict = {}
 
 	pos_exceptions = ["ve", "letter", "cs", "prefix", "suffix", "root", "sandhi", "idiom"]
 
@@ -74,19 +77,26 @@ def setup():
 		if (not re.findall ("!", stem) and 
 		pos not in pos_exceptions and
 		headword_pos not in headwords_pos_count_dict):
-			headwords_pos_count_dict[headword_pos] = {'headword':headword_clean, 'pos': pos, 'root':root_info, 'pattern':pattern, 'inflections': inflections, 'count': 0, 'found':{}}
+			headwords_pos_count_dict[headword_pos] = {'headword':headword_clean, 'pos': pos, 'pattern':pattern, 'root':root_info, 'inflections': inflections, 'count': 0, 'found':{}}
+		
+		if (root != "" and 
+		root_info not in roots_count_dict):
+			roots_count_dict[root_info] = {'inflections': list(inflections), 'count': 0, 'found': {}}
+		if (root != "" and 
+		root_info in roots_count_dict):
+			roots_count_dict[root_info]['inflections'] += list(inflections)
+	
+	return ebṭ_counts_df, dpd_df, all_inflections_dict, sandhi_matches_dict, headwords_pos_count_dict, roots_count_dict
 
-	return ebṭ_counts_df, dpd_df, all_inflections_dict, sandhi_matches_dict, headwords_pos_count_dict
 
-
-ebṭ_counts_df, dpd_df, all_inflections_dict, sandhi_matches_dict, headwords_pos_count_dict = setup()
+ebṭ_counts_df, dpd_df, all_inflections_dict, sandhi_matches_dict, headwords_pos_count_dict, roots_count_dict = setup()
 
 
 def step1_clean_inflection():
 	print(f"{timeis()} {green}finding clean inflections")
 	not_matched_dict = {}
 	ebt_length = len(ebṭ_counts_df)
-	for row in range(2000):  # ebt_length
+	for row in range(ebt_length):  # ebt_length
 		flag = False
 		inflected_word = ebṭ_counts_df.iloc[row, 0]
 		value = ebṭ_counts_df.iloc[row, 1]
@@ -101,16 +111,20 @@ def step1_clean_inflection():
 		if flag == False:
 			not_matched_dict[inflected_word] = value
 
-	print(f"{timeis()} {green}saving csvs")
+		for root in roots_count_dict:
+			if inflected_word in roots_count_dict[root]['inflections']:
+				roots_count_dict[root]['count'] += value
+				roots_count_dict[root]['found'][inflected_word] = value
 
+	print(f"{timeis()} {green}saving csvs")
 
 	not_matched_df = pd.DataFrame.from_dict(not_matched_dict, orient="index")
 	not_matched_df.to_csv("output/not matched.csv", sep="\t", header=None)
 
-	return headwords_pos_count_dict, not_matched_dict
+	return headwords_pos_count_dict, not_matched_dict, roots_count_dict
 	
 
-headwords_pos_count_dict, not_matched_dict = step1_clean_inflection()
+headwords_pos_count_dict, not_matched_dict, roots_count_dict = step1_clean_inflection()
 
 
 def step2_split_sandhis():
@@ -139,6 +153,15 @@ def step2_split_sandhis():
 								
 				if flag == False:
 					still_unmatched_dict[unmatched_word] = value
+
+				for root in roots_count_dict:
+					if sandhi_word in roots_count_dict[root]['inflections']:
+						roots_count_dict[root]['count'] += value
+						if sandhi_word in roots_count_dict[root]['found']:
+							roots_count_dict[root]['found'][sandhi_word] += value
+						else:
+							roots_count_dict[root]['found'][sandhi_word] = value
+
 		counter +=1
 
 	print(f"{timeis()} {green}unmatched {white}{len(not_matched_dict)}")
@@ -146,13 +169,22 @@ def step2_split_sandhis():
 	
 	print(f"{timeis()} {green}still unmatched")
 	print(f"{timeis()} {white}{still_unmatched_dict}")
-	# for key, value in still_unmatched_dict.items():
-	# 	print(f"{timeis()} {white}{key} {value}")
+
+	for headword in headwords_pos_count_dict:
+		headwords_pos_count_dict[headword]['found count'] = len(headwords_pos_count_dict[headword]['found'])
 
 	df = pd.DataFrame.from_dict(headwords_pos_count_dict, orient="index")
 	df.sort_values("count", inplace=True, ascending=False)
 	df.drop("inflections", axis="columns", inplace=True)
 	df.to_csv("output/headword pos count.csv", sep="\t")
+
+	for root in roots_count_dict:
+		roots_count_dict[root]['found count'] = len(roots_count_dict[root]['found'])
+		
+	df2 = pd.DataFrame.from_dict(roots_count_dict, orient="index")
+	df2.sort_values("count", inplace=True, ascending=False)
+	df2.drop("inflections", axis="columns", inplace=True)
+	df2.to_csv("output/roots count dict.tsv", sep="\t")
 
 step2_split_sandhis()
 toc()
